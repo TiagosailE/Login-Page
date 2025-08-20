6712a3a0220453b56ae562f23458de59f346199d
Tiago Santos <tiagosantos.eli@gmail.com>
2025-08-20 14:19:58 -0300

    feat: create login page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideToastr(),
+    provideAnimations(),
+    provideHttpClient(withFetch())
+  ]
 };
